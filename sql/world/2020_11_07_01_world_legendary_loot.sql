ffd14916936badc9e9e75d1e426efa76d632577d
jackhein <moustiquejunior@gmail.com>
2024-01-14 17:56:06 +0100

    Update loot

--- sql/world/2020_11_07_01_world_legendary_loot.sql
+++ sql/world/2020_11_07_01_world_legendary_loot.sql
@@ -74,13 +74,13 @@ SET chance=20
 WHERE Reference = 34024 AND groupId = 1;
 
 UPDATE creature_loot_template
-SET chance=100
-WHERE Reference = 34024 AND groupId = 1 AND Entry = 15339;
+SET chance=100, MaxCount=1
+WHERE Reference = 34024 AND Entry = 15339;
 
 UPDATE creature_loot_template
 SET chance=20, MaxCount=2
 WHERE Reference = 34045;
 
 UPDATE creature_loot_template
-SET chance=100, MaxCount=3
+SET chance=100, MaxCount=2
 WHERE Reference = 34045 AND Entry IN (15727, 15299);
