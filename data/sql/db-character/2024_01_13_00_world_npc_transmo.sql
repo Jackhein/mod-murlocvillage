c24ce6a7aed47ba3778e7852a9580914e54cd32a
jackhein <moustiquejunior@gmail.com>
2024-01-15 15:38:07 +0100

    remove transmog specificity (new forked repository for transmog mod), remove unused conf file

--- data/sql/db-character/2024_01_13_00_world_npc_transmo.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `custom_transmogrification` (`GUID`, `FakeEntry`, `Owner`) select `guid`, `transmog`, `owner_guid` from `item_instance` where `transmog` != 0 and `owner_guid` != 0;
-ALTER TABLE item_instance DROP COLUMN transmog; 
